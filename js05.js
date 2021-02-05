03921eaf8a8a1c207a34f486d898a7246bbacb04
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-05 21:49:37 +0100

    dates

---
+++ js05.js
@@ -0,0 +1,23 @@
+// Dates 
+var d = new Date();
+console.log(d);
+
+var day = new Date(2021,01,02,12,50,30,0);
+console.log(day);
+
+console.log(d.toUTCString());
+
+var dd = new Date("2021-02-25");
+console.log(dd);
+
+var ddd = new Date("2021/02/25");
+console.log(ddd);
+
+var dddd = new Date("Mar 31 2021");
+console.log(dddd);
+
+var a = new Date("January 25 2021");
+console.log(a);
+
+var b = new Date("Jan 05 2021");
+console.log(b);
